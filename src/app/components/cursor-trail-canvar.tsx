b4eb6a39d566ee5afd3ea084512344bbb13d6f2b
HunainAfzal-Dev <hunain.pc933@gmail.com>
2025-01-21 04:05:20 +0500

    cursor trial

--- src/app/components/cursor-trail-canvar.tsx
+++ src/app/components/cursor-trail-canvar.tsx
@@ -1,35 +1,35 @@
-"use client"
+// "use client"
 
-import { CSSProperties, useEffect, useRef } from "react";
-import { cursorTrail } from "../utils/cursor-trail";
+// import { CSSProperties, useEffect, useRef } from "react";
+// import { cursorTrail } from "../utils/cursor-trail";
 
 
-export interface CursorTrailCanvasProps {
-  color?: string;
-  className?: string;
-  style?: CSSProperties;
-}
+// export interface CursorTrailCanvasProps {
+//   color?: string;
+//   className?: string;
+//   style?: CSSProperties;
+// }
 
-export default function CursorTrailCanvas(props: CursorTrailCanvasProps) {
-  const refCanvas = useRef<HTMLCanvasElement>(null);
+// export default function CursorTrailCanvas(props: CursorTrailCanvasProps) {
+//   const refCanvas = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-    const { cleanUp, renderTrailCursor } = cursorTrail({
-      ref: refCanvas,
-      color: props.color,
-    });
-    renderTrailCursor();
+//   useEffect(() => {
+//     const { cleanUp, renderTrailCursor } = cursorTrail({
+//       ref: refCanvas,
+//       color: props.color,
+//     });
+//     renderTrailCursor();
 
-    return () => {
-      cleanUp();
-    };
-  }, [props.color]);
+//     return () => {
+//       cleanUp();
+//     };
+//   }, [props.color]);
 
-  return (
-    <canvas
-      ref={refCanvas}
-      className={props.className}
-      style={props.style}
-    ></canvas>
-  );
-}
+//   return (
+//     <canvas
+//       ref={refCanvas}
+//       className={props.className}
+//       style={props.style}
+//     ></canvas>
+//   );
+// }
