95dae4c6b24c3eeffff525b2afbaa275935a10a9
HunainAfzal-Dev <hunain.pc933@gmail.com>
2025-09-13 02:03:46 +0500

    `Added eslint ignoreDuringBuilds option and type guard to select onChange event handler`

--- src/app/(client)/all-projects/page.tsx
+++ src/app/(client)/all-projects/page.tsx
@@ -264,7 +264,7 @@ const Projects: React.FC = () => {
               
               <select
                 value={sortBy}
-                onChange={(e) => setSortBy(e.target.value as any)}
+                onChange={(e) => setSortBy(e.target.value as "newest" | "oldest" | "title")}
                 className="text-black px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none"
               >
                 <option value="newest">Newest First</option>
